07be69fca70e8eaaf6dd064977c5944121514d43
maxpawgdbs <maxim.taranenko17112008@gmail.com>
2025-06-27 16:18:37 +0300

    update date

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def get():
         return flask.abort(403)
     con = sqlite3.connect("payments.db")
     cur = con.cursor()
-    query = cur.execute("SELECT nickname, date FROM PAYMENTS ORDER BY date").fetchall()
+    query = cur.execute("SELECT nickname, date FROM PAYMENTS ORDER BY date DESC").fetchall()
     cur.close()
     con.close()
     return flask.render_template("gois.html", pays=query)
